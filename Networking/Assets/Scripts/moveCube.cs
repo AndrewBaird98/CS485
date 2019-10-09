4839989aae94565ed0c21e264e892689e18592c0
AndrewBaird98 <baird013@cougars.csusm.edu>
2019-10-08 20:02:43 -0700

    Initial networking setup. Two cubes are able to move independently of each other. I have a .gitignore file but for some reason all the other files are included, sorry.

---
+++ Networking/Assets/Scripts/moveCube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class moveCube : NetworkBehaviour
+{
+   
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    // Update is called once per frame
+
+    public float speed = 20f;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Vector2 pos = this.transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("pressed w");
+
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        this.transform.position = pos;
+    }
+
+}
