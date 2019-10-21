2768701f1b67d60f465ebb82eeb7f8e54ad13e7c
yazeedalota <54645976+yazeedalota@users.noreply.github.com>
2019-10-21 09:03:24 -0700

    Two clients can shoot, apply damages, then destoryed
    Updated Version of Networking

---
+++ Networking2/Networking/Assets/Script/BulletScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script
+{
+    public class BulletScript : MonoBehaviour
+    {
+        private float _lifeTime;
+
+        private static readonly int Color = Shader.PropertyToID("_Color");
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _lifeTime = 5;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _lifeTime -= Time.deltaTime;
+            if (_lifeTime <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            gameObject.GetComponent<Renderer>().material.SetColor(Color, new Color(0.58f, 0f, 0f));
+            var rb = gameObject.GetComponent<Rigidbody>();
+            
+        }
+    }
+}
---
+++ Networking2/Networking/Assets/Script/Shoot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Shoot : NetworkBehaviour
+{
+
+    private float range = 200;
+    private float dmg = 15;
+    [SerializeField] private Transform camTransform;
+    private RaycastHit hit;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CheckifShooting();
+
+    }
+
+
+    void CheckifShooting()
+    {
+
+        if (!isLocalPlayer)
+            return;
+
+        //shots pressing the left mouse click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            shooting();
+
+
+    }
+
+
+    void shooting()
+    {
+
+        if (Physics.Raycast(camTransform.TransformPoint(0, 0, 0.5f), camTransform.forward, out hit, range))
+            Debug.Log(hit.transform.tag);
+
+        if(hit.transform.tag=="Player")
+        {
+            string id = hit.transform.name;
+
+            CmdTellServerWhoWasShot(id, dmg);
+
+        }
+
+    }
+
+    [Command]
+    void CmdTellServerWhoWasShot(string id2, float damage)
+    {
+
+        GameObject go = GameObject.Find(id2);
+
+        //Apply the damage and health
+
+    }
+}
